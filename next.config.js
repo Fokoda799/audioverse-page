2677c6aa1479325e99561235587c6c7c2b5901d0
Fokoda799 <feyt2003@gmail.com>
2026-07-28 02:21:32 +0100

    runtime error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-const createNextIntlPlugin = require("next-intl/plugin");
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{
-      protocol: "https", hostname: "images.unsplash.com",
-      protocol: "https", hostname: "picsum.photos",
-      protocol: "https", hostname: "fonts.googleapis.com",
-      protocol: "https", hostname: "fonts.gstatic.com"
-    }],
-  },
-};
-
-module.exports = withNextIntl(nextConfig);
+import { NextResponse } from "next/server";
+export default function middleware() {
+  return NextResponse.next();
+}
+export const config = { matcher: ["/((?!api|_next|.*\\..*).*)"] };
