298591014e12b7515bd90d3b74b07f18ec066aa6
Fokoda799 <feyt2003@gmail.com>
2026-07-28 02:33:11 +0100

    Update app logo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-import { NextResponse } from "next/server";
-export default function middleware() {
-  return NextResponse.next();
-}
-export const config = { matcher: ["/((?!api|_next|.*\\..*).*)"] };
+const createNextIntlPlugin = require("next-intl/plugin");
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "fonts.googleapis.com" },
+      { protocol: "https", hostname: "fonts.gstatic.com" },
+    ],
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
